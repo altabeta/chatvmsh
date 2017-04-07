2ba0694bc3d1802323089eef2204bc4d85d81a0d
altabeta <weilbrideru@gmail.com>
2017-04-07 14:23:17 +0300

    changed server and client encryption insides;

--- server.py
+++ server.py
@@ -14,35 +14,39 @@ def decodeRSA(text, closed_exponent, n):
     return replacing_words(res)
 
 
-def user_setup():
-    conn, addr = sock.accept()
-    print('Connected with' + addr[0] + ':' + str(addr[1]))
-    open_exponent = int(conn.recv(1024))
-    n = int(conn.recv(1024))
-    keys = RSA()
-    conn.send(keys.open_exponent)  # client gets info to decode messages from server
-    conn.send(keys.n)
-    return open_exponent, n, keys.closed_exponent, keys.n, conn
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
 sock.bind(('', 9000))
 print('Socket binded')
 sock.listen(2)
 print('Socket listening')
-oef, nf, clf, ncl_f, user_f = user_setup()
-oes, ns, cls, ncl_s, user_s = user_setup()
+conn_f, addr_f = sock.accept()
+print('Connected with' + addr_f[0] + ':' + str(addr_f[1]))
+oef = int.from_bytes(sock.recv(1024), 'big')
+ncl_f = int.from_bytes(sock.recv(1024), 'big')
+keysf = RSA()
+conn_f.send((keysf.open_exponent).to_bytes(1024, 'big'))  # client gets info to decode messages from server
+conn_f.send((keysf.n).to_bytes(1024, 'big'))
+conn_s, addr_s = sock.accept()
+print('Connected with' + addr_s[0] + ':' + str(addr_s[1]))
+oes = int.from_bytes(sock.recv(1024), 'big')
+ncl_s = int.from_bytes(sock.recv(1024), 'big')
+keyss = RSA()
+conn_f.send((keyss.open_exponent).to_bytes(1024, 'big'))  # client gets info to decode messages from server
+conn_f.send((keyss.n).to_bytes(1024, 'big'))
+print('Both users are connected')
+'''
 while True:
-    data = user_f.recv(1024)
-    dataS = user_s.recv(1024)
+    data = conn_f.recv(1024)
+    dataS = conn_s.recv(1024)
     if not data and dataS:
         break
-    data = encodeRSA(oef, data, nf)
+    data = encodeRSA(oef, data, ncl_f)
     data = decodeRSA(data, cls, ncl_s)
-    dataS = encodeRSA(oes, dataS, ns)
+    dataS = encodeRSA(oes, dataS, ncl_s)
     dataS = decodeRSA(dataS, clf, ncl_f)
     user_f.send(dataS)
     user_s.send(data)
 sock.close()
 print('Socket closed')
+'''
